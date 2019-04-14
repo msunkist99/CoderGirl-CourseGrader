40ace5172dab6929b600dc38b976da239a6ed7d6
msunkist99 <msunkist99@gmail.com>
2019-04-14 15:00:00 -0500

    20190414 initial code with unit tests

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -2,15 +2,104 @@
 
 namespace CourseGrader
 {
-    public static class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // create a method named GradeTestScores that takes a int array of test scores as its parameter and returns a string
-            // GradeTestScores will add up these test scores and calculate an average score. 
-            // It should then return a string of "pass" or "fail" depending on these two conditions:
-            // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
-            // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+            int[] grades = new int[5];
+
+            // create a loop that builds the grade array from user input
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter test grade #" + (i + 1) + " - ");
+               grades[i] = Convert.ToInt32 (Console.ReadLine());
+            }
+
+            // send the grade array to a method that will
+            // sum the grades in the array
+            // find the average for the grades in the array
+            double gradeAverage = FindAverage(grades);
+            // send the grade array to a method to find the lowest grade
+
+            // send the grade average and the lowest grade to a method
+            int lowestGrade = FindLowestGrade(grades);
+
+            // to determine pass/fail
+            string passFail = GradeTestScores(grades);
+
+            Console.WriteLine($"Average grade is {gradeAverage}.  Lowest grade is {lowestGrade}.");
+            Console.WriteLine(passFail);
+            Console.ReadLine();
+        }
+
+        private static double FindAverage(int[] grades)
+        {
+            int gradeTotal = 0;
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                gradeTotal += grades[i];
+            }
+
+            double gradeAverage = gradeTotal / grades.Length;
+
+            return gradeAverage;
+        }
+
+        private static int FindLowestGrade(int[] grades)
+        {
+            int lowestGrade = grades[0];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (lowestGrade > grades[i])
+                {
+                    lowestGrade = grades[i];
+                }
+            }
+
+            return lowestGrade;
+        }
+
+        public static string GradeTestScores(int[] grades)
+        {
+            string passFail;
+            int lowestGrade = 0;
+            int totalGrades = 0;
+            int averageGrade = 0;
+
+            if (grades.Length == 0)
+            {
+                return "fail";
+            }
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                if (i == 0) 
+                {
+                    lowestGrade = grades[i];
+                }
+
+                if (grades[i] < lowestGrade)
+                {
+                    lowestGrade = grades[i];
+                }
+                totalGrades += grades[i];
+            }
+
+            averageGrade = totalGrades / grades.Length;
+
+            if ((averageGrade > 69) && (lowestGrade > 49))
+            {
+                passFail = "pass";
+            }
+            else 
+            {
+                passFail = "fail";
+            }
+
+            return passFail;
         }
     }
 }
+
